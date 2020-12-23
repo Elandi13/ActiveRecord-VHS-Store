ff9f685254dd7203ead9068c57db95d6993d05d1
Elvis Landi <elandi13@yahoo.com>
2020-12-23 12:14:16 -0500

    finished up until past_due_deliverable

---
+++ app/models/rental.rb
@@ -0,0 +1,40 @@
+class Rental < ActiveRecord::Base
+    belongs_to :vhs
+    belongs_to :client
+
+    
+
+    def due_date
+        self.created_at + 7.days
+    end
+
+    def self.past_due_date
+        binding.pry
+        self.select do |r|
+    
+        end
+
+    end
+
+end
+        
+
+
+
+
+
+# Rental.past_due_date - returns a list of all the rentals past due date, currently rented or rented in the past
+ 
+# past_due = Rental.where (self.due_date < ?, Date.today)
+
+# past_due = self.due_date
+
+#  self.due_date.select("due_date < ?", Date.today)
+# if self.created_at + 7.days == self.updated_at
+#     not overdue
+# elsif self.created_at + 7.days < self.updated_at
+#     overdue
+#     Rental.all.select{|r|r.updated_at > r.due_date}
+
+
+    
